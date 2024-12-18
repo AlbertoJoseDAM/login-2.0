21e01fc3aaacdd6e9c10e16c273bbd886127ce15
Yang Wolfang <jamartin@itpfp.com>
2024-12-18 12:27:53 +0100

    Funcional

--- Formulario/Form2.cs
+++ Formulario/Form2.cs
@@ -15,6 +15,15 @@ namespace Formulario
         public Form2()
         {
             InitializeComponent();
+            label1.Text = $"Bienvenido {Form1.GetUsuario()}, su contraseña es {Form1.GetContraseña()}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+
+            this.Close();
         }
     }
 }
